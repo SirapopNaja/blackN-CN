1b2b9b02d3877b14e9fd84e2f7c3ea5127970216
perm <goon63140@gmail.com>
2018-12-10 19:02:14 +0700

    Add project

---
+++ app/Model/userstd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class userstd extends Model
+{
+    protected $table = 'user';
+    protected $fillable = [
+        'id_user','Pass_user','name','surname','phonenumber','numbercar','email'
+    ];
+}
